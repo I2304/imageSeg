244bb42e6fa36730191daef2fbb6a5defd850a22
Siqiao Mu <siqiaomu@gmail.com>
2020-05-28 08:30:01 -0700

    Change some naming

--- generate_test_img.m
+++ generate_test_img.m
@@ -1,15 +1,21 @@
 %file for generating test images
 img1 = test_img(1, false, 0.25);
-plot_img(img1, ["Test", "Image", "1"])
+plot_img(img1, ["Synthetic", "Image", "1"])
+
+img1 = test_img(1, true, 0.25);
+plot_img(img1, ["Synthetic", "Image", "1", "with", "Noise"])
 
 img2 = test_img(2, false, 0.25);
-plot_img(img2, ["Test", "Image", "2"])
+plot_img(img2, ["Synthetic", "Image", "2"])
+
+img2 = test_img(2, true, 0.25);
+plot_img(img1, ["Synthetic", "Image", "1", "with", "Noise"])
 
 img3 = test_img(3, false, 0.25);
-plot_img(img3, ["Test", "Image", "3"])
+plot_img(img3, ["Synthetic", "Image", "3"])
 
 img4 = test_img(4, false, 0.25);
-plot_img(img4, ["Test", "Image", "4"])
+plot_img(img4, ["Synthetic", "Image", "4"])
 
 function [] = plot_img(img, plot_title)
     figure()
@@ -22,5 +28,5 @@ function [] = plot_img(img, plot_title)
     title (t, 'Interpreter', 'Latex', 'Fontsize', 11)
     file_t = join(plot_title, '_');
     saveas(gcf, join([file_t, ".png"], ''))
-
+    
 end
